76a60530b36d7622b63bb6e32379763660a506d9
nadaso8 <nadaso8th@gmail.com>
2025-01-06 16:30:52 -0600

    setup minimal test env

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs"; # avoids duplicating nixpkgs
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, fenix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        rustToolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-s1RPtyvDGJaX/BisLT+ifVfuhDT1nZkZ1NcK8sbwELM=";
+        };
+        
+	rustPlatform = pkgs.makeRustPlatform {
+          # inherit (rustToolchain) cargo rustc;
+          cargo = rustToolchain.cargo;
+          rustc = rustToolchain.rustc;
+        };
+
+      in
+      {
+        packages.hello = pkgs.hello;
+        packages.default = self.packages.hello;
+
+	devShells.default = pkgs.mkShell {
+
+          buildInputs = (with pkgs; [
+            xorg.libxcb
+            libxml2
+	    llvmPackages_19.mlir
+          ]);
+	
+	  nativeBuildInputs = [
+            rustToolchain
+            rustPlatform.bindgenHook
+	    pkgs.llvmPackages_19.libllvm
+          ];        
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      }
+    );
+}
